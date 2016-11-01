9035631140423f1b9403cb219ee3dde31da82f96
sabercavalry <287967943@qq.com>
2016-11-01 23:51:55 +0800

    fileupload

--- web/src/main/java/com/whitesky/home/controller/PhotoController.java
+++ web/src/main/java/com/whitesky/home/controller/PhotoController.java
@@ -1,17 +1,56 @@
 package com.whitesky.home.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.whitesky.home.controller.common.BaseController;
+import com.whitesky.home.controller.common.GlobalUtil;
 
 @Controller
 @RequestMapping(value = "/photo", method = RequestMethod.GET)
 public class PhotoController extends BaseController {
 
 	@RequestMapping("")
-	public String index(){
+	public String index() {
 		return "/photo/photo";
+	}
+
+	@RequestMapping(value = "save",method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> save(HttpServletRequest request) throws IllegalStateException, IOException {
+		// 将当前上下文初始化给 CommonsMutipartResolver （多部分解析器）
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		// 检查form中是否有enctype="multipart/form-data"
+		if (multipartResolver.isMultipart(request)) {
+			// 将request变成多部分request
+			MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+			// 获取multiRequest 中所有的文件名
+			Iterator<String> iter = multiRequest.getFileNames();
+			while (iter.hasNext()) {
+				// 一次遍历所有文件
+				MultipartFile file = multiRequest.getFile(iter.next().toString());
+				if (file != null) {
+					String path = GlobalUtil.getServletContext().getRealPath("/")+"/res/" + file.getOriginalFilename();
+					// 上传
+					file.transferTo(new File(path));
+				}
+			}
+
+		}
+		return new HashMap<String, Object>();
 	}
 }
--- web/src/main/webapp/WEB-INF/views/photo/photo.jsp
+++ web/src/main/webapp/WEB-INF/views/photo/photo.jsp
@@ -8,14 +8,46 @@
 <body>
 	<jsp:include page="/WEB-INF/views/common/top_nav.jsp?p=photo" />
 	<div class="container">
-		<input id="up_file" type="file" class="file" data-preview-file-type="text" multiple />
+		<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+		  上传图片
+		</button>
 		<jsp:include page="/WEB-INF/views/common/footer.jsp" />
 	</div>
 	<!-- /container -->
+	<!-- Modal -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-lg">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+	        <h4 class="modal-title" id="myModalLabel"> 上传图片</h4>
+	      </div>
+	      <div class="modal-body">
+	        <input id="up_file" name="file" type="file" class="file" multiple />
+	      </div>
+	      <div class="modal-footer">
+	      </div>
+	    </div>
+	  </div>
+	</div>
 	<jsp:include page="/WEB-INF/views/common/js.jsp?fileinput=1" />
 	<script type="text/javascript">
-		$(function() {
-			//上传
+		$("#up_file").fileinput({
+			language:'zh',
+			uploadUrl:'photo/save',
+			allowedFileTypes:['image', 'video', 'audio'],
+			showUpload: true, //是否显示上传按钮
+			showCaption: false,//是否显示标题
+			showClose: false,
+			showUploadedThumbs: false,
+			removeFromPreviewOnError: true,
+			//maxFileSize: 0,//单位为kb，如果为0表示不限制文件大小
+			//minFileCount: 0,
+			maxFileCount: 10, //表示允许同时上传的最大文件个数
+			enctype: 'multipart/form-data',
+			validateInitialCount:true,
+			msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
 		});
 	</script>
 </body>
